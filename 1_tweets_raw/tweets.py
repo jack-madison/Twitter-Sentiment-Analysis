2c7bc7cf903781c77a5f61f9115689eba6246198
Jack Madison <madison@sfu.ca>
2022-06-27 11:58:36 -0700

    upload

--- 1_tweets_raw/tweets.py
+++ 1_tweets_raw/tweets.py
@@ -14,8 +14,8 @@ symptoms_tweets = []
 for response in tweepy.Paginator(client.search_all_tweets, 
                                 query = '-is:retweet -is:nullcast has:geo place_country:JP',
                                 tweet_fields = ['author_id', 'created_at', 'geo', 'id', 'lang', 'public_metrics', 'source', 'text'],
-                                start_time = '2018-04-22T00:00:00+09:00',
-                                end_time = '2018-04-30T23:59:59+09:00',
+                                start_time = '2018-06-22T00:00:00+09:00',
+                                end_time = '2018-06-30T23:59:59+09:00',
                                 max_results=500):
     time.sleep(1)
     symptoms_tweets.append(response)
@@ -38,4 +38,4 @@ for response in symptoms_tweets:
 # Change this list of dictionaries into a dataframe
 tweets = pd.DataFrame(result)
 
-tweets.to_csv('./1_tweets_raw/2018/apr_w4_2018.csv', index = False)
+tweets.to_csv('./1_tweets_raw/2018/jun_w4_2018.csv', index = False)

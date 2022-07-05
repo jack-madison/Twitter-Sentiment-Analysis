6e5413e35551536c72f29e36245c6a73fb1e2a4b
Jack Madison <madison@sfu.ca>
2022-07-05 11:40:11 -0700

    update

--- 1_tweets_raw/tweets.py
+++ 1_tweets_raw/tweets.py
@@ -5,17 +5,17 @@ import tweepy
 # If this code has been downloaded from github, please create a new Python file called
 # twitter_authentication containing bearer_token = "INSERT YOUR BEARER TOKEN HERE" in the
 # same directory as this Python file 
-from twitter_authentication import bearer_token_1
+from twitter_authentication import bearer_token_5
 
-client = tweepy.Client(bearer_token_1, wait_on_rate_limit=True)
+client = tweepy.Client(bearer_token_5, wait_on_rate_limit=True)
 
 symptoms_tweets = []
 
 for response in tweepy.Paginator(client.search_all_tweets, 
                                 query = '-is:retweet -is:nullcast has:geo place_country:JP',
                                 tweet_fields = ['author_id', 'created_at', 'geo', 'id', 'lang', 'public_metrics', 'source', 'text'],
-                                start_time = '2017-04-01T00:00:00+09:00',
-                                end_time = '2017-04-07T23:59:59+09:00',
+                                start_time = '2017-04-22T00:00:00+09:00',
+                                end_time = '2017-04-30T23:59:59+09:00',
                                 max_results=500):
     time.sleep(1)
     symptoms_tweets.append(response)
@@ -34,10 +34,10 @@ for response in symptoms_tweets:
                     'tweet_location_id': tweet.geo['place_id']
                     })
         except TypeError:
-            print("Type Error")
+            pass
         except KeyError:
-            print("Key Error")
+            pass
 # Change this list of dictionaries into a dataframe
 tweets = pd.DataFrame(result)
 
-tweets.to_csv('./1_tweets_raw/2017/apr_w1_2017.csv', index = False)
+tweets.to_csv('./1_tweets_raw/2017/apr_w4_2017.csv', index = False)

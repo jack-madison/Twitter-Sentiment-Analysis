28e50344b4725e52919c20136f802f37489bf9ef
Jack Madison <madison@sfu.ca>
2022-07-07 14:27:24 -0700

    update

---
+++ 5_graphing/pollen_counts_map.R
@@ -0,0 +1,59 @@
+library(ggplot2)
+library(sp)
+library(raster)
+library(broom)
+library(tidyverse)
+library(gpclib)
+library(rgeos)
+library(readr)
+library(haven)
+
+library(rgdal)
+library(maptools)
+if (!require(gpclib)) install.packages("gpclib", type="source")
+gpclibPermit()
+
+
+jpn2 <- getData("GADM", country = "JPN", level = 2)
+
+jpn2_df <- tidy(jpn2, region = "NAME_2")
+
+jpn2_df$id <- str_replace_all(jpn2_df$id, "ō", "o")
+jpn2_df$id <- str_replace_all(jpn2_df$id, "Ō", "O")
+jpn2_df$id <- str_replace_all(jpn2_df$id, "ū", "u")
+
+id <- jpn2_df %>% group_by(id) %>% summarise(long = mean(long), lat = mean(lat))
+
+oseti_tweets <- read_dta("GitHub/Twitter-Sentiment-Analysis/4_tweets_aggregated/twitter_mun_cleaned2.dta")
+
+oseti_tweets <- oseti_tweets[!is.na(oseti_tweets$pollen_t999), ]
+
+tweet_counts <- oseti_tweets %>% group_by(mun_id) %>% summarise(pollen = mean(pollen_t999), long = mean(mun_X), lat = mean(mun_Y))
+
+library(geosphere)
+mat <- distm(id[,c('long','lat')], tweet_counts[,c('long','lat')], fun=distVincentyEllipsoid)
+id$pollen <- tweet_counts$pollen[apply(mat, 1, which.min)]
+
+id <- subset(id, select = c(id, pollen))
+
+df <- merge(jpn2_df, id, by = 'id')
+
+my_breaks = c(0, 30, 100, 300, 1000, 3000)
+
+library(scales) 
+
+library(readxl)
+pollen_station_master <- read_excel("GitHub/Twitter-Sentiment-Analysis/5_graphing/pollen_station_master.xlsx")
+
+pollen_station_master <- subset(pollen_station_master, last_year >= 2019)
+
+ggplot() + geom_polygon(data = df, aes(x = long, y = lat, group = group, fill = pollen)) + geom_point(data = pollen_station_master, aes(x = longitude, y = latitude), col="red", size=1) + theme_void() + coord_equal(xlim = c(127, 147), ylim = c(30,46)) + scale_fill_gradient(high = "#02b818", low = "#9cf7a7", name = "Average Daily Pollen in 2019", trans = "log", labels = comma, breaks = my_breaks) + ggtitle("Average Daily Pollen Counts in Japan (2019)")
+
+
+
+
+
+
+
+
+

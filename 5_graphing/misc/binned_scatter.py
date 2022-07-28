8351eb18b2d92f420280c0354f09bcd40e6e284f
Jack Madison <madison@sfu.ca>
2022-07-28 15:10:26 -0700

    update

--- 5_graphing/misc/binned_scatter.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import statsmodels.formula.api as smf
-from plotnine import *
-from binsreg import *
-
-df = pd.read_stata('./4_tweets_aggregated/twitter_mun_cleaned2.dta')
-
-est = binsreg(x = 'ln_pollen_t999', y = 'mean_oseti_score1', data = df)

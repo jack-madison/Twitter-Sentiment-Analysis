2c4526d8fde6eae04916d894ebc5fc6b646cca85
Jack Madison <madison@sfu.ca>
2022-08-31 23:10:57 -0700

    update

--- 6_graphing/chart_graph/chart_graph.R
+++ 6_graphing/chart_graph/chart_graph.R
@@ -18,7 +18,7 @@ xx <- seq(-0.1,0.6,0.1)
 # Read in the mean column from the data frame
 br <- estimates$Mean
 
-# Set the colour of the points to be plotted
+### Set the colour of the points to be plotted ###
 colz = rep("black", length(tp))
 
 # Read in the category and group labels
@@ -40,24 +40,32 @@ cat_locs <- c(18.5, 15.5, 12.5, 9.5, 6.5, 2.5)
 
 #Specify the output name and figure dimensions
 ### If the figure is too spaced out or squashed vertically, change the height variable ###
-pdf(file="./figure.pdf",width=9,height=6,useDingbats = F)
+pdf(file="./figure.pdf", width=9, height=6, useDingbats = F)
 
 # Set the margins
-par(mgp=c(2,0.8,0),mar=c(5,4,4,3),lend=1)
+par(mgp=c(2,0.8,0), mar=c(5,4,4,3), lend=1)
 
 # Set the plot options
 ### To change the x axis label, change the xlab text ###
 ### In order to have the graph portion of the table be scaled correctly you will have to adjust the xlim() options ###
 ### If the graph is too narrow, reduce the range of the xlim. If the graph is too wide, increase the xlim range ###
-plot(1, type="n", xlim=c(-1.1,0.8), ylim=c(1,dim(estimates)[1]+1), axes=F, ylab="", xlab="                                                                                              % Change in Vehicle Accidents", cex.lab = 0.65)
+### For whatever reason, the base R plot package does not let you manually position axis labels. In order to move the ###
+### axis label to be centered on the axis, spaces must be added before the title ###
+plot(1, type="n", xlim=c(-1.1,0.8), ylim=c(1,dim(estimates)[1]+1), axes=F, ylab="", 
+     xlab = "                                                                                           % Change in Vehicle Accidents", cex.lab = 0.65)
+
 
 # More plot options
 abline(v=xx, lwd=0.5, col="grey", lty=2)
 abline(v=0, lwd=0.5)
 segments(cilo, nn, cihi, nn)
-points(tp, nn, pch=21, bg=colz, cex = 0.65)
 abline(h=lz, lwd=0.75, lty=1, col="grey")
 axis(1, at=xx, labels=xx, cex.axis=0.65)
+
+
+### The size of the points by changing cex and the symbol using pch, a list of the symbols can be found online ###
+points(tp, nn, pch=21, bg=colz, cex = 0.65)
+
 
 # Place the text columns
 ### You will have to play around with the first number in each of the text() options ###
